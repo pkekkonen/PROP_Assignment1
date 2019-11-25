629c50839fdccd3416b877a24d786928af42c79f
Ida <idso0102@dsv.su.se>
2019-11-25 14:34:48 +0100

    Update part_one.js

--- part_one.js
+++ part_one.js
@@ -19,23 +19,24 @@ myObject.call = function(funcName, parameters) {
 
 	//TODO: gör koll om prototypes lista finns 
 	this.prototypes.forEach(proto => {
-		while(!funcNameFound) {
 			if(proto.hasOwnProperty(funcName) && typeOf(proto.funcName === 'function')) { //förutsätter att funcname är inskickad som String
-
+				
 			} else {
 				var parentProto = proto.__proto__;
 				while(parentProto != null) {
-					if(parentProto.hasOwnProperty(funcName)) {
+					if(parentProto.hasOwnProperty(funcName) && typeof(parentProto.funcName === 'function')) {
+						
 						//break;
+					} else {
+						
+					parentProto = parentProto.__proto__;
 					}
-
-					parentProto = proto.__proto__;
 				}
 
 			}
 			//kolla om proto har funktionen
 			//om inte så leta uppåt i dess prototyper
 		}
-	};);
+	);
 
 };
