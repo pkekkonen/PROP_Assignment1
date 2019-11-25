2c71e93ffedaf136d83fa1822d39bf8d5b102c29
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-11-25 13:14:20 +0100

    Implemented function stubs

---
+++ part_one.js
@@ -0,0 +1,17 @@
+//create a object myObject with create method
+//call(funkName, parametee
+
+var myObject = {
+	var prototypes = [];
+
+};
+
+myObject.create = function(prototypeList) {
+	prototypeList = prototypes;
+
+};
+
+
+myObject.call = function(funcName, parameters) {
+
+};
