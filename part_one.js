4ebf7c7e539a4e6e260f96c868408aa94eb46b69
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-11-26 09:56:07 +0100

    Fixed some problems

--- part_one.js
+++ part_one.js
@@ -1,13 +1,17 @@
 //create a object myObject with create method
 //call(funkName, parametee
 
-var myObject = {
-	var prototypes = [];
+var myObject = {};
 
-};
-
+//måste se till att myObject blir proto till objektet som anropar den
 myObject.create = function(prototypeList) {
-	prototypeList = prototypes;
+	var obj = Object.create(myObject);
+	prototypes = [];
+	obj.prototypes = prototypeList;
+	return obj;
+
+	//this berör myObject objektet, inte objektet som anropade den
+
 	// variabler? hittas inte i prototyp
 
 };
@@ -16,44 +20,49 @@ myObject.create = function(prototypeList) {
 myObject.call = function(funcName, parameters) {
 	//börjar söka igenom element 0 i protoypes, sedan dess grand-prototypes
 	//finns den inte i det ledet så rör vi oss till element 1 etc.
-	search(this.prototypes);
-	//TODO: gör koll om prototypes lista finns 
-
-
+	search(this.prototypes,funcName, parameters);
+	//TODO: gör koll om prototypes lista finns. Fast vad menar jag med det för den måste väl finnas eftersom myObject är en prototyp till objektet 
 };
 
 
-search = function(protos) {
-	protos.forEach(currentProto => {
+search = function(protos, funcName, parameters) {
+
+	//ska först kolla i själva objektet man anropar med
+	for(var i = 0; i < protos.length; i++) {
+			var currentProto = protos[i];
 			var parentProtos = getParentPrototypes(currentProto);
 
-			if(proto.hasOwnProperty(funcName) && typeOf(currentProto.funcName) === 'function') { //förutsätter att funcname är inskickad som String
-				return //vadå?
+			if(currentProto.hasOwnProperty(funcName)) { //förutsätter att funcname är inskickad som String
+				//&& typeOf(currentProto.funcName) === 'function'. Måste kolla att det faktiskt är en funktion som vi kan anropa
+				console.log("här ");
+				return currentProto[funcName](parameters); //varför returnar denna inte hela vägen??
 			} else {
 				parentProtos.forEach(currentParentProto =>{
-					if(parentProto.hasOwnProperty(funcName) && typeof(parentProto.funcName) === 'function') {
-						return //vadå?
-						//break;
+
+					if(parentProto.hasOwnProperty(funcName)) {
+						//&& typeOf(currentProto.funcName) === 'function'. Måste kolla att det faktiskt är en funktion som vi kan anropa
+						return parentProto[funcName](parameters);
 					}
 				});
-
 				//när vi har nått null så vill vi röra oss ett steg nedåt och ett steg åt höger i dess prototypes lista förutsatt att den har en 
 				var i = parentProtos.length;
 				do{ 
 					i--;
 					lastParenProto = parentProtos[i];
-				} while (typeOf(lastParenProto) !== myObject  && i !== 0);
+				} while (lastParentProto.__proto__ !== myObject  && i !== 0);
 
-				search(lastParenProto.prototypes);
-				//rekursivt anrop
+				var result = search(lastParenProto.prototypes);
+				if(result !== undefined)
+					return result;
+				//om den är undefined så betyder det att den aldrig hittade någon funktion i det rekursiva anropet och det betyder att vi ska röra oss till nästa element i prototypesList
 
 			}
 			//kolla om proto har funktionen
 			//om inte så leta uppåt i dess prototyper
-		}
-	);
-}
+	}
 
+	return undefined;
+}
 
 //måste vi kolla typen? Checka att proto verkligen är en object??
 getParentPrototypes = function(proto) { 
@@ -61,7 +70,37 @@ getParentPrototypes = function(proto) {
 
 	while(proto.__proto__ != null) {
 			parentProtos.push(proto.__proto__);
+			proto = proto.__proto__;
 	}
 
 	return parentProtos;
 }
+
+var obj1 = {};
+obj1.hej = function() {console.log("hej")};
+var obj2 = {};
+obj2.hej = function() {console.log("hello")};
+var obj3 = myObject.create([obj2]);
+obj3.hej = function() {console.log("hola")};
+var result = obj3.call("hej", []);
+
+console.log();
+console.log("NEXT");
+var obj4 = myObject.create([obj1, obj2, obj3]);
+var result = obj4.call("hej", []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
