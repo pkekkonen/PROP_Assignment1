ac2a73488865b28f9fc2e2223b5c4b3ccfa85c68
Ida <idso0102@dsv.su.se>
2019-11-27 10:49:09 +0100

    Kollar ens föräldraklass

--- part_two.js
+++ part_two.js
@@ -9,27 +9,35 @@ createClass = function(className, superClassList) {
 	obj.className = className;
 	obj.superClassList = (superClassList != null? superClassList: []);
 
+	console.log(className + "     " + superClassList);
 	return obj;
 }
 
 templateClass.new = function() {
 	var obj = Object.create(this);
-
 	return obj;
 }
 
 templateClass.call = function(funcName, parameters) {
 	if(this.hasOwnProperty(funcName)) {
+		console.log(funcName);
 		return this[funcName](parameters);
 	} 
+	if(this.__proto__.hasOwnProperty(funcName)) {
+		return this.__proto__[funcName](parameters);
+	}
 	return search(this.superClassList, funcName, parameters);
 }
 
 search = function(superClassList, funcName, parameters) {
-	for(var i; i < superClassList.length; i++){
+	for(var i = 0; i < superClassList.length; i++){
 		var currentSuperClass = superClassList[i];
+		console.log(currentSuperClass + "Här är jag")
 		if(currentSuperClass.hasOwnProperty(funcName)) {
-			return currentSuperClass[funcName](parameters);
+			var result = currentSuperClass[funcName](parameters);
+			if (result != undefined){
+			return result;
+			}
 		}
 		var result = search(currentSuperClass.superClassList, funcName, parameters);
 		if(result !== undefined){
@@ -48,7 +56,7 @@ class2.func = function(arg) { return "func2: " + arg; };
 var class3 = createClass("Class3", [class1, class2]);
 var obj3 = class3.new();
 var result = obj3.call("func", ["hello"]);
-console.log(result);
+console.log("’result’ is assigned ’func0: hello’  "+result);
 //where ’result’ is assigned ’func0: hello’.
 
 class0 = createClass("Class0", null);
@@ -57,8 +65,9 @@ class1 = createClass("Class1", [class0]);
 class2 = createClass("Class2", []);
 class3 = createClass("Class3", [class2, class1]);
 obj3 = class3.new();
+obj3.name = "obj3";
 result = obj3.call("func", ["hello"]);
-console.log(result);
+console.log("’result’ is assigned ’func0: hello’  "+result);
 //where ’result’ is assigned ’func0: hello’.
 
 
@@ -67,5 +76,5 @@ class0 = createClass("Class0", null);
 class0.func = function(arg) { return "func0: " + arg; };
 var obj0 = class0.new();
 result = obj0.call("func", ["hello"]);
-console.log(result);
+console.log("’result’ is assigned ’func0: hello’  "+result);
 //where ’result’ is assigned ’func0: hello’.
