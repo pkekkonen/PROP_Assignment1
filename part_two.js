7e9d2589a84253d6aae46fedc7bf993a54d5892d
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-11-27 09:58:08 +0100

    Started implementing basic functionality for creating classes

---
+++ part_two.js
@@ -0,0 +1,23 @@
+// https://ilearn2.dsv.su.se/mod/forum/discuss.php?d=45930
+// ^ ang. den förvirrande meningen i uppgiftsbeskr.
+
+
+var templateClass = {};
+
+createClass = function(className, superClassList) {
+	var obj = Object.create(templateClass);
+	obj.className = className;
+	obj.superClassList = (superClassList !== null? superClassList: []);
+
+	return obj;
+}
+
+templateClass.new = function() {
+	var obj = Object.create(this);
+
+	return obj;
+}
+
+templateClass.call = call(funcName, parameters) {
+
+}
