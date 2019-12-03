c57fdf7afdaee0ff23b8c158c297f471685f125a
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-12-03 10:37:26 +0100

    Update part_two.js

--- part_two.js
+++ part_two.js
@@ -7,24 +7,25 @@ createClass = function(className, superClassList) {
 	newClass.isClass = true;
 
 	var superClasses = (superClassList != null? superClassList : []);
+
 	Object.defineProperty(newClass, "superClasses", { 
 		get : function() { 
 				var newArray = superClasses.slice();
-				return newArray;},
+				return newArray; },
 		set : function(classToAdd) { 
 
-		if(!classToAdd.hasOwnProperty("isClass")){
-			throw "This parameter is not a class.";
-		} else if(newClass === classToAdd) {
-			throw "Cannot add class as superclass to itself.";	
-		} else if(searchAfterSuperClass(newClass.superClasses, classToAdd.className)) {
-			throw "Not adding  " + classToAdd.className+ " as a superclass since it already is.";
-			
-		} else if(searchAfterSuperClass(classToAdd.superClasses, newClass.className)) {
-			throw "Cannot add " + classToAdd.className +" as a superclass since it will cause circular inheritance.";
-		} else {
-			superClasses.push(classToAdd);
-		}}
+				if(!classToAdd.hasOwnProperty("isClass")){
+					throw "This parameter is not a class.";
+				} else if(newClass === classToAdd) {
+					throw "Cannot add class as superclass to itself.";	
+				} else if(searchAfterSuperClass(newClass.superClasses, classToAdd.className)) {
+					throw "Not adding  " + classToAdd.className + " as a superclass since it already is.";
+				
+				} else if(searchAfterSuperClass(classToAdd.superClasses, newClass.className)) {
+					throw "Cannot add " + classToAdd.className + " as a superclass since it will cause circular inheritance.";
+				} else {
+					superClasses.push(classToAdd);
+				}}
 
 	});
 
@@ -34,7 +35,9 @@ createClass = function(className, superClassList) {
 	}
 
 	newClass.addSuperClass = function(classToAdd) {
-		this.superClasses = (classToAdd);
+		this.superClasses = classToAdd;
+
+
 	}
 
 	newClass.new = function() {
@@ -91,37 +94,5 @@ searchAfterSuperClass = function(superClassList, searchedClassName) {
 	}
 	return false;
 }
-
-
-var class0 = createClass("Class0", null);
-var class1 = createClass("Class1", null);
-var class2 = createClass("Class2", null);
-var class3 = createClass("Class3", null);
-var class4 = createClass("Class4", [class0, class1, class2]);
-
-console.log(class4.getSuperClassList())
-
-p = class4.getSuperClassList();
-
-
-console.log(p)
-p.push(class3);
-class4.superClasses.push(class3)
-
-console.log(class4.superClasses)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
