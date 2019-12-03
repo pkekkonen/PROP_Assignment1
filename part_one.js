a37a9ef2f8a01f3a4dc11c937d594d122dfe49c8
Ida <idso0102@dsv.su.se>
2019-12-03 12:03:40 +0100

    Update part_one.js

--- part_one.js
+++ part_one.js
@@ -33,7 +33,7 @@ myObject.create = function(prototypeList) {
 		}
 
 	obj.addPrototype = function(objToAdd) {
-		set(objToAdd);
+		this.prototypes = (objToAdd);
 	}
 	 
 	return obj;
@@ -64,7 +64,7 @@ searchAfterFunction = function(protos, funcName, parameters) {
 				return result;
 		} else {
 			if(currentProto.hasOwnProperty("hasPrototypes")) {
-				var result = searchAfterFunction(currentProto.getPrototypes, funcName, parameters);
+				var result = searchAfterFunction(currentProto.getPrototypes(), funcName, parameters);
 				if(result !== undefined) {
 					console.log("hej2   "+result);
 					return result;
@@ -98,12 +98,10 @@ searchAfterPrototype = function(protos, searchedObject) {
 
 
 //TESTKOD
-
 var obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
 var obj1 = myObject.create([obj0]);
-var obj2 = myObject.create([]);
-obj2.func = function(arg) { return "func2: " + arg; };
-var obj3 = myObject.create([obj1, obj2]);
-var result = obj3.call("func", ["hello"]) ;
-console.log("should print ’func0: hello’ ->", result);
+obj0.getPrototypes().push(obj1);
+
+console.log("result  "+ obj0.getPrototypes());
+
+
